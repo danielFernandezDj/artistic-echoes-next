69b4eeda3a8cfdb8050134d108dbf26173422b6d
Daniel Fernandez <mdanif9728@gmail.com>
2024-12-22 16:54:24 -0800

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
-
-// import ImageCard from "@/Components/ui/ImageCard"
-import { MongoClient } from "mongodb";
 
 export default async function Home() {
-
-
   return (
     <main>
 
